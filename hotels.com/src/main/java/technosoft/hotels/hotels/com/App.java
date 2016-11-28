7e5bbc01656e6a1def94311b7376be5c47e3a6e7
gsi0011 <gsi0011@outlook.com>
2016-11-28 10:05:11 -0500

    created maven project

---
+++ hotels.com/src/main/java/technosoft/hotels/hotels/com/App.java
@@ -0,0 +1,13 @@
+package technosoft.hotels.hotels.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
