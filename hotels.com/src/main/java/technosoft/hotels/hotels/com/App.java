b4c7009645b5d788aee6a253860673255b72caaf
gsi0011 <gsi0011@outlook.com>
2016-11-29 14:08:45 -0500

    sample webdriver change

--- hotels.com/src/main/java/technosoft/hotels/hotels/com/App.java
+++ hotels.com/src/main/java/technosoft/hotels/hotels/com/App.java
@@ -1,13 +1,32 @@
 package technosoft.hotels.hotels.com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	WebDriver driver = null;
+	private static String URL = "https://www.facebook.com/";
+    public void ChromeDriverpick(){
+    	System.setProperty("webdriver.chrome.driver", "WebDrivers/chromedriver.exe");
+    	driver = new ChromeDriver();
+    }
+    
+    
+    @BeforeMethod
+    public void chooseDriver(){
+    	ChromeDriverpick();
+    }
+    
+    @Test
+    public void gotoHome() throws InterruptedException{
+    	driver.navigate().to(URL);
+    	Thread.sleep(5000);
     }
 }
