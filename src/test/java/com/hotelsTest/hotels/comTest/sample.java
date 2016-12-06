5abf97d595c14b4c5812fba3a666d2bbbe29c58e
gsi0011 <gsi0011@outlook.com>
2016-12-06 12:29:14 -0500

    updated ExcelFileReader in DataProviders Class

--- src/test/java/com/hotelsTest/hotels/comTest/sample.java
+++ src/test/java/com/hotelsTest/hotels/comTest/sample.java
@@ -9,8 +9,8 @@ public class sample {
 //extends WebDriverFactory {
 
 	@Test(dataProviderClass = DataProviders.class, dataProvider = "test")
-	public void testMe(String a, String b, String c, String d) throws Exception{
-		System.out.println(a + b+ c+ d);
+	public void testMe(String a, String b, String c, String d,String  e,String  f,String  g,String  h,String  i ,String  j ,String  k, String l) throws Exception{
+		System.out.println(a+b+c+d+e+f+g+h+i+j+k+l);
 		
 		
 	}
