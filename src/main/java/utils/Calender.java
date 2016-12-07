52e3ac5cc24043dcf7ca58c88ab677ba921bce9f
gsi0011 <gsi0011@outlook.com>
2016-12-07 10:20:13 -0500

    Calender updated

--- src/main/java/utils/Calender.java
+++ src/main/java/utils/Calender.java
@@ -1,5 +1,35 @@
 package utils;
 
-public class Calender {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import org.openqa.selenium.By;
 
+public class Calender extends WebDriverFactory{
+	
+
+
+	
+	
+	public static void selectTomorrow(){
+		String tomorrow = addDaysToCurrentDate(1);
+		getDriver().findElement(By.cssSelector("#qf-0q-localised-check-in")).click();//locater needed here
+		getDriver().findElement(By.cssSelector("a[aria-label='"+ tomorrow+ "']")).click();//static
+	}
+	
+	public static void selectDayAfterTomorrow(){
+		String dayAfterTomorrow = addDaysToCurrentDate(2);
+		getDriver().findElement(By.cssSelector("#qf-0q-localised-check-out")).click();// locater needed here
+		getDriver().findElement(By.cssSelector("a[aria-label='"+ dayAfterTomorrow+ "']")).click();
+	}
+	
+	public static String addDaysToCurrentDate(int days) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, days); 
+        
+        SimpleDateFormat timeFormat = new SimpleDateFormat("MM/dd/yy");
+        String currentTime = timeFormat.format(calendar.getTime());
+        return currentTime;
+	}
+	
+	
 }
--- src/test/java/com/hotelsTest/hotels/comTest/sample.java
+++ src/test/java/com/hotelsTest/hotels/comTest/sample.java
@@ -1,16 +1,26 @@
 package com.hotelsTest.hotels.comTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import utils.Calender;
 import utils.DataProviders;
 import utils.WebDriverFactory;
 
-public class sample {
+public class sample{
 //extends WebDriverFactory {
-
-	@Test(dataProviderClass = DataProviders.class, dataProvider = "test")
-	public void testMe(String a, String b, String c, String d,String  e,String  f,String  g,String  h,String  i ,String  j ,String  k, String l) throws Exception{
-		System.out.println(a+b+c+d+e+f+g+h+i+j+k+l);
+	
+	@Test
+	public void testMe() throws InterruptedException{
+//		Calender.selectTomorrow();
+//		Calender.selectDayAfterTomorrow();
+	//	Calender.addDays(20);
+		Thread.sleep(10000);
+		
+		//Calender.pickdate();
+		
+		
 		
 		
 	}
